9f4f830b02cf3f454c9920f44523a1425421a8de
haiseong <wjdgotjd9908@gmail.com>
2023-06-01 20:00:32 +0900

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+#Web VPython 3.2
+from vpython import *
+
+# 게임 세팅 초기화
+# 예) 공, 라켓, 테이블, 스코어 등
+
+dt = 0.01
+
+# 메인 루프
+while True:
+    # 공, 라켓 위치 등 초기화
+    while True:
+        rate(1/dt)
+        # 사용자 입력 처리
+
+        # 게임 로직 업데이트
+
+        # 그래픽 업데이트
+
+    # 스코어 처리
