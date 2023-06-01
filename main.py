fb7822b2d15c7b5cdb7967c491424693c86f30f5
haiseong <wjdgotjd9908@gmail.com>
2023-06-01 20:23:52 +0900

    [feat] 테이블 객체 생성

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from vpython import *
 # 게임 세팅 초기화
 # 예) 공, 라켓, 테이블, 스코어 등
 
+
+# 탁구 테이블 객체 생성
+table_length = 2.74
+table_width = 1.525
+table_height = 0.05
+table = box(pos=vector(0, -table_height / 2, 0), size=vector(table_length, table_height, table_width), color=color.blue)
+
+
 dt = 0.01
 
 # 메인 루프
