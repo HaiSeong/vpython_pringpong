e99727516c51139b66819c9ad71e1e32a8d35ea6
haiseong <wjdgotjd9908@gmail.com>
2023-06-01 20:24:13 +0900

    [feat] 테이블 라인 그리기

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ table_width = 1.525
 table_height = 0.05
 table = box(pos=vector(0, -table_height / 2, 0), size=vector(table_length, table_height, table_width), color=color.blue)
 
+table_lines = [
+    box(pos=vector(0, -table_height / 2, 0), size=vector(0.02, table_height+0.0001, table_width+0.0001), color=color.white),
+    box(pos=vector(table_length/2 - 0.01, -table_height / 2, 0), size=vector(0.02+0.0001, table_height+0.0001, table_width+0.0001), color=color.white),
+    box(pos=vector(-table_length/2 + 0.01, -table_height / 2, 0), size=vector(0.02+0.0001, table_height+0.0001, table_width+0.0001), color=color.white),
+    box(pos=vector(0, -table_height / 2, table_width/2 - 0.01), size=vector(table_length, table_height+0.0001, 0.02+0.0001), color=color.white),
+    box(pos=vector(0, -table_height / 2, -table_width/2 + 0.01), size=vector(table_length, table_height+0.0001, 0.02+0.0001), color=color.white),
+]
 
 dt = 0.01
 
