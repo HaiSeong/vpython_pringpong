1b31231e311c09ba0f3121c0e378e20088acf2de
haiseong <wjdgotjd9908@gmail.com>
2023-06-01 20:08:27 +0900

    공 객체 생성

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from vpython import *
 # 게임 세팅 초기화
 # 예) 공, 라켓, 테이블, 스코어 등
 
+# 공 객체 생성
+ball_radius = 0.05  # 공의 반지름
+ball = sphere(pos=vector(0, 0, 0), radius=ball_radius, color=color.orange)# 공의 질량 설정
+ball.mass = 0.0027
+
 dt = 0.01
 
 # 메인 루프
